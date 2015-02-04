bc8f9f37e011e9233dcf78916ad6776582c36391
martineaua <info@alain-martineau.com>
2015-02-04 15:26:24 +0100

    installation soap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new HB\BlogBundle\HBBlogBundle(),
+        	new BeSimple\SoapBundle\BeSimpleSoapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
